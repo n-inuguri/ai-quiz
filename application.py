51c90deded113d4dcc65faf18a9a946bb446afa1
n-inuguri <inuguri@gmail.com>
2025-12-09 18:41:27 +0000

    updates

--- application.py
+++ application.py
@@ -41,7 +41,7 @@ def main():
         st.session_state.custom_persona = ""
         
 
-    st.title("AI QUIZ GENERATOR 🧠❓")
+    st.title("AI QUIZ GENERATOR")
 
     st.sidebar.header("Model Configuration")
 
